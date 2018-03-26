bd439bc9cb18b4577c1abebfc23ddadedb2c43e1
stefan.jovanovic <stefan.jovanovic@zone47-ec.cs.ucalgary.ca>
2018-03-26 11:27:10 -0600

    working node server with clients

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var socketPeople = [];
+var totalClients = 0;
+
 app.get('/', function(req, res){
   //res.sendFile(__dirname + '/fabric.js');
-  res.sendFile(__dirname + '/drawingMode.html');
+  if(totalClients === 0){
+    res.sendFile(__dirname + '/drawingMode.html');
+    totalClients ++;
+  }
+  else{
+    res.sendFile(__dirname + '/listenClient.html');
+    totalClients ++;
+  }
+
 });
 
 //check if the user is connected or disconnected
@@ -15,6 +26,11 @@ io.on('connection', function(socket){
   //when the user disconnects take him off the onlineUser list
   socket.on('disconnect', function(){
     console.log('user disconnected');
+
+  });
+  socket.on('push data', function(data){
+    io.emit('receive data', data);
+    console.log("pushed the dtat to clients");
   });
 });
 
